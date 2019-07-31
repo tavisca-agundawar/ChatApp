406c8347bf7b1a363d86c0da0e057f3604500cb9
tavisca-agundawar <52607030+tavisca-agundawar@users.noreply.github.com>
2019-07-31 11:17:38 +0530

    ChatAppLauncher

---
+++ ChatApplicationLauncher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatApp;
+
+namespace ChatApplicationLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainProgram mainProgram = new MainProgram();
+            mainProgram.Run();
+        }
+    }
+}
